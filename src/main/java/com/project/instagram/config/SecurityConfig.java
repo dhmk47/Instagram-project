3f6e2d650e88e1f9741f5d2d5ce2b66085593216
dhmk47 <dhmk47@naver.com>
2022-12-30 19:19:30 +0900

    인스타그램 메인페이지 작성

--- src/main/java/com/project/instagram/config/SecurityConfig.java
+++ src/main/java/com/project/instagram/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.project.instagram.config;
 
 import com.project.instagram.config.auth.CustomFailureHandler;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.jni.Local;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @EnableWebSecurity
 @Configuration
@@ -25,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
+
 
         http.authorizeRequests()
                 .anyRequest()
